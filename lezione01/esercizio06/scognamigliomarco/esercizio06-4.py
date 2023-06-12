dc14a5727df2af84adedcacb4eff00a700e6996f
4CI-Scognamiglio-Marco-2022-2023 <19576@studenti.marconiverona.edu.it>
2023-06-12 15:46:08 +0200

    esercizio06

--- lezione01/esercizio06/scognamigliomarco/esercizio06-4.py
+++ lezione01/esercizio06/scognamigliomarco/esercizio06-4.py
@@ -1,7 +1,7 @@
 numero = int(input("inserire un numero compreso tra 1 e 5"))
 
 if (numero >= 1 and numero <=5):
-    if (numero == 2 or numero == 3):
+    if (numero == 2 or numero == 3 or numero == 5):
         print ("il numero", numero, "è un numero primo")
     else:
         print ("il numero", numero, "non è un numero primo")
