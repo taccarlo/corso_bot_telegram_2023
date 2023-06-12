42a7b5d3fa5a4075cd10d0daf7f8d0187dbb49ea
4CI-Scognamiglio-Marco-2022-2023 <19576@studenti.marconiverona.edu.it>
2023-06-12 16:14:06 +0200

    esercizio bello

--- lezione01/esercizio07/lavagna/esercizio.py
+++ lezione01/esercizio07/lavagna/esercizio.py
@@ -7,6 +7,16 @@ def numeriDispari():
         list.append(x)
     print(list, " ")
 
+
+def bello():
+    print("Il tuo numero è tra i seguenti?")
+    list = []
+    for x in range(16, 32):
+        list.append(x)
+    for x in range(48, 63):
+        list.append(x)
+    print (list)
+
 # prima domanda (1,3,5)
 numeriDispari()
 # seconda domanda (partendo da 2 scrivo 2 numeri sì e 2 no) PRIMO
@@ -16,5 +26,5 @@ numeriDispari()
 # quarta domanda (partendo da 8 scrivo 8 numeri sì e 8 no) GIULIA
 
 # quinta domanda (partendo da 16 scrivo 16 numeri sì e 16 no) BELLO
-
+bello()
 # sesta domanda (partendo da 32 scrivo 32 numeri sì e 32 no) TRE NEURONI SOMMATI
