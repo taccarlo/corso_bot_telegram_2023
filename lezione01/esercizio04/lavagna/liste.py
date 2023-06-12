ed15f259a06b2abfa8d16960a3cfc48795357f61
JackBranca <brancaleonigiacomo@liceomedi.com>
2023-06-12 12:09:36 +0200

    es

--- lezione01/esercizio04/lavagna/liste.py
+++ lezione01/esercizio04/lavagna/liste.py
@@ -3,10 +3,4 @@
     lista che scambi le occorrenze di una lettera con un'altra.
 """
 
-lista =[]
-i=0
-while i<10:
-    val = input("Inserisci un valore: ")
-    lista = lista + [val]
-    i=i+1
-print("Hai inserito: ",lista)
+
