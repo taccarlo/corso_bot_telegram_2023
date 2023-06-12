b92fbe2c62a9b4e72ca87cac3bf79ed8cc0e7732
Carlo Tacchella <tacchella.carlo@gmail.com>
2023-06-12 11:54:53 +0200

    [UP] testo modificato

---
+++ lezione01/esercizio04/lavagna/liste.py
@@ -0,0 +1,8 @@
+"""
+Scrivere e testare una funzione sostituisci(lista,valore1,valore2)
+che prende in input una lista e due valori e ritorna una lista in cui
+ tutte le occorrenze del valore1 sono sostituite da valore2
+Esempio :   
+print(sostituisci([‘p’,’i’,’p’,’p’,’o’],‘p’,‘l’)) 
+>>> [‘l’,’i’,’l’,’l’,’o’] 
+"""
