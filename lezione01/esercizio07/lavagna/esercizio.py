1a7d5841da0c634919567f4ede78219738a1249b
Carlo Tacchella <tacchella.carlo@gmail.com>
2023-06-12 16:05:31 +0200

    Create esercizio.py

---
+++ lezione01/esercizio07/lavagna/esercizio.py
@@ -0,0 +1,20 @@
+
+
+def numeriDispari():
+    print("Il tuo numero è tra i seguenti?")
+    list = []
+    for x in range(1, 63,2):
+        list.append(x)
+    print(list, " ")
+
+# prima domanda (1,3,5)
+numeriDispari()
+# seconda domanda (partendo da 2 scrivo 2 numeri sì e 2 no)
+
+# terza domanda (partendo da 4 scrivo 4 numeri sì e 4 no)
+
+# quarta domanda (partendo da 8 scrivo 8 numeri sì e 8 no)
+
+# quinta domanda (partendo da 16 scrivo 16 numeri sì e 16 no)
+
+# sesta domanda (partendo da 32 scrivo 32 numeri sì e 32 no)
