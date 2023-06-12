886cf8ec8a5b13089e906354c2668368f221c2d2
Carlo Tacchella <tacchella.carlo@gmail.com>
2023-06-12 11:56:16 +0200

    Update liste.py

--- lezione01/esercizio04/lavagna/liste.py
+++ lezione01/esercizio04/lavagna/liste.py
@@ -1,8 +1,12 @@
 """
-Scrivere e testare una funzione sostituisci(lista,valore1,valore2)
-che prende in input una lista e due valori e ritorna una lista in cui
- tutte le occorrenze del valore1 sono sostituite da valore2
-Esempio :   
-print(sostituisci([‘p’,’i’,’p’,’p’,’o’],‘p’,‘l’)) 
->>> [‘l’,’i’,’l’,’l’,’o’] 
+    Prendere in input una lista da tastiera e ritornare una
+    lista che scambi le occorrenze di una lettera con un'altra.
 """
+
+lista =[]
+i=0
+while i<10:
+    val = input("Inserisci un valore: ")
+    lista = lista + [val]
+    i=i+1
+print("Hai inserito: ",lista)
