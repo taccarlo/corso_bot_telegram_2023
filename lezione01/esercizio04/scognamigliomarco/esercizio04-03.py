2db7e6608a18926857667cf8f4fc023a596db0ac
4CI-Scognamiglio-Marco-2022-2023 <19576@studenti.marconiverona.edu.it>
2023-06-12 12:39:29 +0200

    esercizio 4

--- lezione01/esercizio04/scognamigliomarco/esercizio04-03.py
+++ lezione01/esercizio04/scognamigliomarco/esercizio04-03.py
@@ -1,11 +1,12 @@
 lista = []
 i=0
 while i<5:
-    val = input("inserisci una lettera: ")
+    val = int(input("inserisci un numero: "))
     lista = lista + [val]
     i+=1
 print (lista)
 
+controllo = True
 i=0
 valoreminimo = lista[0]
 valoremedia = 0
@@ -13,6 +14,8 @@ valoremassimo = lista[0]
 while i<5:
     if (type[lista[i]] != int ):
         print ("ERRORE")
+        controllo = False
+        break
     else:
         if (valoreminimo > lista[i]):
             valoreminiomo = lista[i]
@@ -20,3 +23,7 @@ while i<5:
         if (valoremassimo < lista[i]):
             valoremassimo = lista[i]
 valoremedia = valoremedia/5
+
+if (controllo == True):
+    listaTot[valoreminimo, valoremedia, valoremassimo]
+    print (listaTot)
