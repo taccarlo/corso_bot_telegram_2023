fea6f9ec19727b7fc74781f28652dd50243a9bf9
GitHubCunego <136308936+GitHubCunego@users.noreply.github.com>
2023-06-15 12:43:53 +0200

    Update Algoritmo_vita_CV.py

--- Algoritmo_vita_CV.py
+++ Algoritmo_vita_CV.py
@@ -5,6 +5,24 @@ anno_superiori = 1
 b = 0
 
 #creazione funzioni#
+#def anni lacvoro#
+def anni_lavoro():
+    print("lavoro")
+    c = 2000
+    for i in range(0,19):
+        c = c+1
+        print("anno di lavoro", c)
+    
+    z = input("INFORTUNIO DEBILITANTE ?")
+    if z == "vero" or z == "Vero":
+        print("pensione")
+    else:
+        print("ultimi anni di lavoro")
+        w = input("quante promozioni")
+        if w >= "3":
+            print("capo d'azienda"),print("pensione")
+        else:
+            print("pensione") 
 #calcolo bocciature
 def bocciature():
     global anno_superiori
@@ -17,11 +35,12 @@ def bocciature():
         if b == 0:
             anno_superiori = anno_superiori + 1   
     
-        print("Lavoro!")
-"""FUNZIONE SUL LAVORO"""
+    print("Lavoro!")
+        
         
 #svolgimrnto#
 print("La persna è nata")
 print("La persona segue elementari e medie")
 bocciature()
+anni_lavoro()
     
