ba06a1a5fb8f2a0348d9940d05d515d5f030b345
4GI-BERGAMO-GIULIA-2022-2023 <91021790+4GI-BERGAMO-GIULIA-2022-2023@users.noreply.github.com>
2023-06-15 10:02:13 +0200

    Create primaparte.py

---
+++ lezione02/algoritmodellamicizia/primaparte.py
@@ -0,0 +1,12 @@
+def rosso():   
+    numero= input("inserisci numero di telefono")
+    si=1
+    no=0
+    risposta=int(input("sei in casa? inserisci una risposta se si digitare 1 se no digitare 0"))
+    if risposta==1:
+        print("ti va di mangiare qualcosa insieme?")
+        if risposta==1:
+            print("mangiate qualcosa")
+        else:      
+    else:
+        print("lascia un messaggio")
