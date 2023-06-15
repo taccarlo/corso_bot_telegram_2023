347e9116c4442f10a564e4a860faed85319106f4
4GI-BERGAMO-GIULIA-2022-2023 <91021790+4GI-BERGAMO-GIULIA-2022-2023@users.noreply.github.com>
2023-06-15 12:27:08 +0200

    Create rosa.py

---
+++ lezione02/algoritmodellavita/rosa.py
@@ -0,0 +1,8 @@
+def rosa():
+    print("sono gli ultimi anni di lavoro")
+    promozioni=int(input("quante volte sei sato promosso"))
+    if promozioni>3:
+        print("sei capo della azienda, vai in pensione")
+    else:
+        print("pensione")
+        
