d3038f0f2b9929893631a40c2b367cb8abefd578
RiccardoBarraco <136308491+RiccardoBarraco@users.noreply.github.com>
2023-06-15 10:02:00 +0200

    Create def_amicizia.py

---
+++ lezione02/algoritmodellamicizia/def_amicizia.py
@@ -0,0 +1,16 @@
+def rossa():
+    d=input("Componi il numero di telefono della persona: ")
+    r=input("La persona è a casa? (S/N) ")
+    if r ==("n") or r==("N") :
+        print("Lascio un messaggio, aspetto di essere richiamato")
+    else:
+       d1= input("ti va di mangiare qualcosa assieme?(S/N) ")
+       if d1==("N") or d1==("n"):
+           blu
+       else:
+            print("Mangiate qualcosa assieme")
+            print("Ora siete diventati amici!!")
+            
+print(rossa())
+        
+        
