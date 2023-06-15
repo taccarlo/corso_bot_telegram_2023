ce473ec9a42f73a6671cb1554039c14a350354a2
19661M <136309006+19661M@users.noreply.github.com>
2023-06-15 14:21:36 +0200

    Update gruppo_ber.py

--- lezione02/algoritmodellavita/gruppo_ber.py
+++ lezione02/algoritmodellavita/gruppo_ber.py
@@ -23,3 +23,28 @@ def funzione2():
         print("PENSIONE")
     else:
         funzione3()
+
+def funzione1():
+    annoSuperiori = 0
+    bocciature = 0 # numero di bocciature
+
+    print("La persona è nata.")
+    print("Seguo l'anno %s anno delle superiori" % annoSuperiori)
+    while annoSuperiori < 5 and bocciature < 3:
+
+        print("Seguo l'anno %s anno delle superiori e sono stato bocciato %s volte" % (annoSuperiori, bocciature))
+        bocciato = int(input("bocciato\n1 si 0 no\n") ) # se è stato bocciato si o no
+
+        if bocciato == 1:
+
+            bocciature+=1
+            print("Seguo l'anno %s anno delle superiori e sono stato bocciato %s volte" % (annoSuperiori, bocciature))
+
+            if bocciature > 3:
+                funzione2
+
+        else:
+            annoSuperiori+=1
+    funzione2
+
+funzione1()
