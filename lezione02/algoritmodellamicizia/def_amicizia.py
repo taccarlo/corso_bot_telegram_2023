a3272078e47365d9190ed86dddf24bf93d9c39c2
NicolaPreato <preatonicola@liceomedi.com>
2023-06-15 10:08:34 +0200

    Update def_amicizia.py

--- lezione02/algoritmodellamicizia/def_amicizia.py
+++ lezione02/algoritmodellamicizia/def_amicizia.py
@@ -1,3 +1,16 @@
+def blu():
+    r=input("E di bere qualcosa di caldo?(S/N) ")
+    if r == ("N"):
+        verde()
+    else:
+        c = input("Scegli: ")
+        if c == ("Tè"):
+            print("Fatevi 'sto tè")
+        elif c == ("Caffè"):
+            print("Fatevi 'sto caffè")
+        elif c == ("Cicciolata"):
+            print("fatevi 'sta cioccolata")
+        print("SIETE DIVENTATTI AMICI")
 def rossa():
     d=input("Componi il numero di telefono della persona: ")
     r=input("La persona è a casa? (S/N) ")
@@ -6,7 +19,7 @@ def rossa():
     else:
        d1= input("ti va di mangiare qualcosa assieme?(S/N) ")
        if d1==("N") or d1==("n"):
-           blu
+           blu()
        else:
             print("Mangiate qualcosa assieme")
             print("Ora siete diventati amici!!")
