c25268910a0212ed9597eaaedf47cdcd08159677
Tommidane <136308596+Tommidane@users.noreply.github.com>
2023-06-15 12:16:03 +0200

    multicolor

---
+++ algoritmo_multicolor.py
@@ -0,0 +1,8 @@
+def algoritmo_multicolor():
+    i=0
+    while(i<20):
+        Risposta1=input ("Infortunio debilitante? S o N.\n")
+        if (Risposta1=="S"):
+            print ("Vai in pensione")
+        if (Risposta1=="N"):
+            return ("N")
