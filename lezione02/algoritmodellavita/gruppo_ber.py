b5e2f85e9adccb834d9c19f257ad614422aa3d6c
4CI-Scognamiglio-Marco-2022-2023 <19576@studenti.marconiverona.edu.it>
2023-06-15 12:15:59 +0200

    implemantata funzione3

---
+++ lezione02/algoritmodellavita/gruppo_ber.py
@@ -0,0 +1,7 @@
+def funzione3():
+    print("ultimi anni di lavoro")
+    promozioni = int(input("Quante promozioni? "))
+    if (promozioni > 3):
+        print("capo di azienda")
+    else:
+        print("pensione")
