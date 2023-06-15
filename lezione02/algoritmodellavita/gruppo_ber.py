57e9a4a37a6265d1047580fd926af74f42971dc3
alesioli <136309090+alesioli@users.noreply.github.com>
2023-06-15 12:39:42 +0200

    Update gruppo_ber.py

--- lezione02/algoritmodellavita/gruppo_ber.py
+++ lezione02/algoritmodellavita/gruppo_ber.py
@@ -1,7 +1,16 @@
-def funzione3():
-    print("ultimi anni di lavoro")
-    promozioni = int(input("Quante promozioni? "))
-    if (promozioni > 3):
-        print("capo di azienda")
+def funzione2():
+    print ("Inizio a lavorare")
+    i = 0
+    infortunio = bool(False)
+    while i < 20 and infortunio == False:
+        i += 1
+        print("Ho completato il %d anno di lavoro" %i)
+        j = input("Mi sono infortunato? ")
+        j.lower()
+        if j == "si":
+            infortunio = True
+        
+    if infortunio:
+        print("Sono andato in pensione")
     else:
-        print("pensione")
+        funzione3()
