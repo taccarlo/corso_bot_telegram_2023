a007c2ce28331f66162d11095014eb3fe3c3f421
Carlo Tacchella <tacchella.carlo@gmail.com>
2023-06-16 12:25:32 +0200

    Update prova_db.py

--- lezione03/esercizio02/lavagna/prova_db.py
+++ lezione03/esercizio02/lavagna/prova_db.py
@@ -7,9 +7,8 @@ print("Opened database successfully");
 
 try:
     cursor = db.cursor()
-    #query = "SELECT Data, Ora,Wind  from  dump_dati_stazioni_VR where wind > 300"
-    #query = "select name, Milliseconds from tracks where Milliseconds<180000 ORDER BY Name DESC limit 3"
-    query = "select * from tracks inner join albums on tracks.AlbumId = albums.AlbumId limit 10"
+    query ="select * from tracks limit 10"
+    #query = "select * from tracks inner join albums on tracks.AlbumId = albums.AlbumId limit 10"
     cursor.execute(query)
     result = cursor.fetchall()
     print(result)
