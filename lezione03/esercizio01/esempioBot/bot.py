55a7ca2a29a37813924be02c2668b62f646a4c9b
Carlo Tacchella <tacchella.carlo@gmail.com>
2023-06-16 10:43:42 +0200

    Update bot.py

--- lezione03/esercizio01/esempioBot/bot.py
+++ lezione03/esercizio01/esempioBot/bot.py
@@ -1,5 +1,6 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
 
 # IMPORTANTE: inserire il token fornito dal BotFather nella seguente stringa
 with open("token.txt", "r") as f:
@@ -9,10 +10,21 @@ with open("token.txt", "r") as f:
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
+async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text_caps = ' '.join(context.args).upper()
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+    
 def main():
+
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("hello", hello))
+    app.add_handler(CommandHandler("echo", echo))
+    app.add_handler(CommandHandler('caps', caps))
+    app.add_handler(echo_handler)
     app.run_polling()
 
 
