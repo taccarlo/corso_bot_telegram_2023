de412eef28179ee6231997221f9d808e2778b7e4
VittoriaTagliani <vittoria.tagliani@gmail.com>
2023-06-16 09:46:19 +0200

    vi

---
+++ lezione03/esercizio01/Vitto18bot/bot.py
@@ -0,0 +1,20 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+# IMPORTANTE: inserire il token fornito dal BotFather nella seguente stringa
+with open("5777554355:AAH5yzWbVfag5HKXyENS7m3rKsC9rVR_OF0", "r") as f:
+    TOKEN = f.read()
+    print("Il tuo token è ", TOKEN)
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("hello", hello))
+    app.run_polling()
+
+
+if __name__=='__main__':
+   main()
