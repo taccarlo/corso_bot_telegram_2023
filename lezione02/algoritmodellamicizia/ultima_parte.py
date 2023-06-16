9cbe1809077aebc42404fd1f3ebf78e1bae01d5a
Simonaangelasangarajageappuhamy <122974340+Simonaangelasangarajageappuhamy@users.noreply.github.com>
2023-06-16 09:42:05 +0200

    aggiorno

---
+++ lezione02/algoritmodellamicizia/ultima_parte.py
@@ -0,0 +1,24 @@
+def ultima_parte ():
+    i=0
+    s=1
+    n=0
+    print ('ALLORA SVEGLIAMOCI UN PO')
+    print('cos\'altro ti va di fare ?')
+    domanda=input(' è una cosa che va di fare anche a me ?')
+    while i<7:
+        if (domanda==s):
+            print ('FACCIAMOLO INSIEME,DAI')
+            i=3
+            print('svagatevi un po\' insieme')
+        elif (domanda==n):
+           print('cos\'altro ti va di fare ?')
+            
+                
+        break
+        
+    print ('scegli fra tutte le opzioni  quella che ti appare meno disumana')
+    print ('fattela piacere')
+    i=3
+    
+
+ultima_parte()
