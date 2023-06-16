7de0f672683add437d680f0af092bd9da278f2cc
KevinBalasca <kevinbalasca@gmail.com>
2023-06-16 12:41:30 +0200

    Update bot.py

--- lezione03/esercizio01/Vitto18bot/bot.py
+++ lezione03/esercizio01/Vitto18bot/bot.py
@@ -9,10 +9,14 @@ with open("token.txt", "r") as f:
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
+async def nuova():
+    await pdate.message.reply_text('E\' il migliore')
+
 
 def main():
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("hello", hello))
+    app.add_handler(CommandHandler("maignan", maignan))
     app.run_polling()
 
 
